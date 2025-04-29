b0b6e7f7ac64e07733a7e6b18effc98db2ab7aa1
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-29 19:18:18 +0530

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers:100,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html',{open:'always',outputFolder},],['line']],
+  reporter: 'html',
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
